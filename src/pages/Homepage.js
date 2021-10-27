af9503159012d1b7a213862bf60a78dba5e2d54e
Antoinedebes <antoinedebes@gmail.com>
2021-10-27 17:42:11 +0300

    feat: Added the slider and fixed some stuffs

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,17 +1,49 @@
 import React from "react";
 import Image from "../assets/images/electricity-power-plant.jpg";
+import Gatekeeper from "../assets/images/wp-gatekeeper.jpg";
+import House from "../assets/images/wp-house.jpeg";
+import Hut from "../assets/images/wp-hut.jpeg";
+import Policeman from "../assets/images/wp-policeman.jpeg";
+import Studio from "../assets/images/wp-studio.jpeg";
+import TruckDriver from "../assets/images/wp-truck-driver.jpeg";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import "./Homepage.css";
 
 function Homepage() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
   return (
     <>
       <div className="wrapper__hero__container">
         <div className="hero__container__img">
-          <img src={Image} alt="Zoummar village" />
-        </div>
-        <div>
-          <h1>Where your dreams come true</h1>
-          <button>Get started</button>
+          <Slider {...settings}>
+            <div>
+              <img src={TruckDriver} alt="Zoummar village" />
+            </div>
+            <div>
+              <img src={Image} alt="Zoummar village" />
+            </div>
+            <div>
+              <img src={Gatekeeper} alt="Zoummar village" />
+            </div>
+            <div>
+              <img src={House} alt="Zoummar village" />
+            </div>
+            <div>
+              <img src={Hut} alt="Zoummar village" />
+            </div>
+            <div>
+              <img src={Policeman} alt="Zoummar village" />
+            </div>
+          </Slider>
         </div>
       </div>
     </>
