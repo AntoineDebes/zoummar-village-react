ca6b073ec850a484decc20928ae017443f1d1fcc
Antoinedebes <antoinedebes@gmail.com>
2021-10-28 14:54:44 +0300

    feat: Created activities page

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import "./normalize.css";
 import "./App.css";
 import Header from "./components/Header";
 import { BrowserRouter } from "react-router-dom";
-import Homepage from "./pages/Homepage";
+// import Homepage from "./pages/Homepage";
 import Footer from "./components/Footer";
+import Activities from "./pages/Activities";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="wrapper" id="wrapper">
         <Header />
-        <Homepage />
+        <Activities />
         <Footer />
       </div>
     </BrowserRouter>
