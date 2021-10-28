5e15f36f790ac41463253bde8cab4a6a31194931
Antoinedebes <antoinedebes@gmail.com>
2021-10-28 15:58:44 +0300

    fix: Changed the cards color to white and the body background to darker white and made ythe cards border with radius

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import "./normalize.css";
 import "./App.css";
 import Header from "./components/Header";
 import { BrowserRouter } from "react-router-dom";
-// import Homepage from "./pages/Homepage";
 import Footer from "./components/Footer";
-import Activities from "./pages/Activities";
+// import Activities from "./pages/Activities";
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="wrapper" id="wrapper">
         <Header />
-        <Activities />
+        <Homepage />
         <Footer />
       </div>
     </BrowserRouter>
